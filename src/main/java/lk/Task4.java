56b9f23f41b477049e58e97b535c887fd263af0b
KD ShalomHosheya Jayathilaka <129625075+Shalomhosheya@users.noreply.github.com>
2025-12-03 10:55:14 +0530

    Fouth task done

---
+++ src/main/java/lk/Task4.java
@@ -0,0 +1,27 @@
+package lk;
+
+public class Task4 {
+    public static void main(String[] args) {
+        int[] ar = {50, 2, 1, 9};
+        sorting(ar);
+    }
+
+    private static void sorting(int[] ar) {
+        int temp;
+
+        for (int i = 0; i < ar.length - 1; i++) {
+            for (int j = 0; j < ar.length - i - 1; j++) {
+                
+                if (ar[j] < ar[j + 1]) {
+                    temp = ar[j];
+                    ar[j] = ar[j + 1];
+                    ar[j + 1] = temp;
+                }
+            }
+        }
+
+        for (int i = 0; i < ar.length; i++) {
+            System.out.println(ar[i]);
+        }
+    }
+}
