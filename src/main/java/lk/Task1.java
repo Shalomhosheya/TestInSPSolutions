60e0f3dc609be226bbd4932643842355b901aff6
KD ShalomHosheya Jayathilaka <129625075+Shalomhosheya@users.noreply.github.com>
2025-12-03 10:28:00 +0530

    first task done

--- src/main/java/lk/Task1.java
+++ src/main/java/lk/Task1.java
@@ -1,7 +1,53 @@
 package lk;
 
-public class Main {
+import java.util.ArrayList;
+
+public class Task1 {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+
+        System.out.print("For loop sum: ");
+        forloop(list);
+
+        System.out.print("While loop sum: ");
+        forwhile(list);
+
+        System.out.print("Recursion sum: ");
+        System.out.println(recursion(list, 0));
+    }
+
+    private static void forloop(ArrayList<Integer> list) {
+        int total = 0;
+
+        for (int i = 0; i < list.size(); i++) {
+            total += list.get(i);
+        }
+
+        System.out.println(total);
+    }
+
+    private static void forwhile(ArrayList<Integer> list) {
+        int i = 0;
+        int total = 0;
+
+        while (i < list.size()) {
+            total += list.get(i);
+            i++;
+        }
+
+        System.out.println(total);
+    }
+
+    private static int recursion(ArrayList<Integer> list, int index) {
+
+        if (index == list.size()) {
+            return 0;
+        }
+
+        return list.get(index) + recursion(list, index + 1);
     }
 }
