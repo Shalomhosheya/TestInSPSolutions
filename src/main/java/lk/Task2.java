18e6f07b95ca27f4b88ac9e68211c14ebcf39fca
KD ShalomHosheya Jayathilaka <129625075+Shalomhosheya@users.noreply.github.com>
2025-12-03 10:39:24 +0530

    second task done

---
+++ src/main/java/lk/Task2.java
@@ -0,0 +1,35 @@
+package lk;
+
+import java.util.ArrayList;
+
+public class Task2 {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>();
+
+        list.add("a");
+        list.add("b");
+        list.add("c");
+        list.add("d");
+
+        ArrayList<Integer> list2 = new ArrayList<>();
+        list2.add(1);
+        list2.add(2);
+        list2.add(3);
+        list2.add(4);
+
+        String combine = combineList(list,list2);
+        
+
+
+
+    }
+
+    private static String combineList(ArrayList<String> list, ArrayList<Integer> list2) {
+        String combined = "";
+        for (int i = 0; i < list.size(); i++) {
+             combined = list.get(i) + list2.get(i);
+            System.out.println(combined);
+        }
+        return combined;
+    }
+}
